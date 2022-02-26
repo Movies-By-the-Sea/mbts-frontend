451053883ae76f2b496c1775754ed40212e7ce39
Saumya Bhatt <saumya.bhatt106@gmail.com>
2022-02-26 22:03:06 +0530

    Grammatical updates

--- src/components/Page/AboutPage/About.jsx
+++ src/components/Page/AboutPage/About.jsx
@@ -9,8 +9,8 @@ export default function About() {
         <Page img={img} alt={img} info="No Time To Die, Cary Joji Fukunaga">
             <h1 className="about-title">Name’s Bhatt. Saumya Bhatt</h1>
             <div className="about-text">
-                <p>Cue gun barrel theme... Not really. I’m just an avid movie fan and like every other bloke, felt the need to share my opinion on them to the masses. Coupled with a weak spot for artistic film posters and behold! MBtS was born!</p>
-                <p>My Dad, an avid movie lover himself has a cabinate full of DVDs. It had all the classics. Maybe that's what got me started into movies. I’m by no means a film critique. But then again, what does a cinephile with time on his hand do?</p>
+                <p>Cue gun barrel theme... Not really. I’m just an avid movie fan and like every other guy, felt the need to share my opinion on them to the masses. Coupled with a weak spot for artistic film posters and behold! MBtS was born!</p>
+                <p>My Dad, an avid movie lover himself has a cabinate full of DVDs. It had all the classics. Maybe that's what got me started into movies. I’m by no means a film critic. But then again, what does a cinephile with time on his hand do?</p>
             </div>
         </Page>
     )
