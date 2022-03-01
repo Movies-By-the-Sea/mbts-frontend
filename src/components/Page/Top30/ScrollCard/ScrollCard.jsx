7b06d5d0c89b7ce2b01a3ec202d770181124e905
Saumya Bhatt <saumya.bhatt106@gmail.com>
2022-03-01 16:26:35 +0530

    Made Top30 components responseive

--- src/components/Page/Top30/ScrollCard/ScrollCard.jsx
+++ src/components/Page/Top30/ScrollCard/ScrollCard.jsx
@@ -1,32 +1,49 @@
+import { useMediaQuery } from 'react-responsive';
+
 import './scrollCard.css';
 
-export default function scrollCard({data, handleOpen}) {
+export default function ScrollCard({data, handleOpen}) {
+
+    const isMobile = useMediaQuery({query:'(max-width:600px)'});
 
     return (
         <div className='t3-scroll-parent' >
           {data.map((item, index) => (
               <div className="sc-box">
-                <img 
-                key       = {item.name}
-                alt       = {item.name}
-                src       = {item.poster}
-                className = "sc-img"
-                />
+                {isMobile ? (<></>) : (
+                  <img 
+                  key       = {item.name}
+                  alt       = {item.name}
+                  src       = {item.poster}
+                  className = "sc-img"
+                  />
+                )}
                 <div className="sc-text">
                   <div className="sc-title">{index + 1}. {item.name}</div>
                   <p className="sc-subtitle">
-                    MBTS Score: {item.ratings.overall} / 5  
+                    {isMobile ? (`${item.ratings.overall} / 5`) : (`MBTS Score: ${item.ratings.overall} / 5`)}  
                     <div className="info-divider">|</div>
-                      {item.genre.map((i)=>i.name + ' ')}
-                    <div className="info-divider">|</div>
-                      {item.year}
+                    {item.genre.map((i)=>i.name + ' ')}
+                    {isMobile ? (<></>) : (
+                      <>
+                        <div className="info-divider">|</div>
+                        {item.year}
+                      </>
+                    )}
                   </p>
+                  {isMobile ? (
+                    <img 
+                    key       = {item.name}
+                    alt       = {item.name}
+                    src       = {item.poster}
+                    className = "sc-img"
+                    />
+                  ) : (<></>)}
                   <p>Directed by {item.director}. Starring {item.actors.map((i) => i.name + ', ')}</p>
                   <div className="sc-action">
                     <div 
-                      style     = {{marginRight:"20px"}}
                       onClick   = {handleOpen(item)}
-                      className = "sc-button">Read More</div>
+                      className = "sc-button btn-left">Read More</div>
                     <a 
                       target = {"_blank"}
                       rel    = "noreferrer"
--- src/components/Page/Top30/Top30.jsx
+++ src/components/Page/Top30/Top30.jsx
@@ -7,7 +7,9 @@ import ScrollCard from './ScrollCard/ScrollCard';
 import Modal from '../../common/Modal/Modal';
 
 export default function Top30() {
+    
     const img = "https://ik.imagekit.io/mbts/top30_nnzfgf-gm.png?ik-sdk-version=javascript-1.4.3&updatedAt=1646124049610";
+    const alt = "https://ik.imagekit.io/mbts/top30mob_rUIwxy1GF.png?ik-sdk-version=javascript-1.4.3&updatedAt=1646131895099"
 
     const [loading, setLoading] = useState(true);
     const [films, setFilms] = useState([]);
@@ -43,7 +45,7 @@ export default function Top30() {
 
       return loading ? (<Loading />) : (
           <>
-          <Page img={img} alt={img}>
+          <Page img={img} alt={alt}>
               <ScrollCard data={films} handleOpen={openModal}/>
           </Page>
           {modal ? <Modal handleModal={closeModal} info={inFocus} /> : <></>}
