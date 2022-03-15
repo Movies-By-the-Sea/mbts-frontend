9fdf289d9d5241bbd20db96809f6fe46aa40e00a
Saumya Bhatt <saumya.bhatt106@gmail.com>
2022-03-15 12:15:12 +0530

    Changed scroll-view to flip-view for collections page

--- src/components/Page/CollectionsPage/Collections.jsx
+++ src/components/Page/CollectionsPage/Collections.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react';
 import { mapping } from './mapping';
 import { Page } from '../../common/Page';
 import { useNavigate } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
+import Flippy, { FrontSide, BackSide } from 'react-flippy';
 
-import ScrollBar from '../../common/ScrollBar/ScrollBar';
 import './collections.css';
 
 export default function Collections() {
@@ -13,13 +12,8 @@ export default function Collections() {
     name    : "Browse Collection",
     bg_info : "Requiem For A Dream, Darren Arronofsky",
     bg      : "https://ik.imagekit.io/mbts/collections_ucwl3bPOi.png?ik-sdk-version=javascript-1.4.3&updatedAt=1645937449246",
-    subtitle: "Scroll or use arrow keys to select an experience from below and discover more such films."
+    subtitle: "Select an experience from below and discover more such films."
   }
-
-    const [bgTitle, setBgTitle]       = useState(defaultBg.name);
-    const [bgSubtitle, setBgSubtitle] = useState(defaultBg.subtitle);
-    const [bg, setBg]                 = useState(defaultBg.bg);
-    const [bgInfo, setBgInfo]         = useState(defaultBg.bg_info)
 
     const isMobile = useMediaQuery({query:'(max-width:600px)'});
     const navigate = useNavigate();
@@ -29,41 +23,40 @@ export default function Collections() {
         navigate(`/collections/${item.link}`);
     }
 
-    const handleHover = (item) => (e) => {
-      setTimeout(() => {
-        e.preventDefault();
-        setBgTitle(item.name);
-        setBgInfo(item.bg_info);
-        setBgSubtitle(item.subtitle);
-        setBg(item.bg);
-      }, 500);
-    }
-
   return (
-      <Page img={bg} alt={bg} info={bgInfo}>
-          <h1>{bgTitle}</h1>
-          <p>{bgSubtitle}</p>
-          {isMobile ? (
-            mapping.map((card) => {
-                return (<img 
-                src       = {card.poster}
-                alt       = {card.name}
-                key       = {card.name}
-                onClick   = {openPage(card)}
-                className = "card-img-mob" />)
-            })
-          ) : ( 
-              <div className='scroll-collection-box'>
-                <ScrollBar
-                data      = {mapping}
-                openModal = {openPage}
-                className = "collection-card"
-                advanced={true}
-                setHover={handleHover}
-                defaultState={defaultBg}
-                />
-              </div>
-        )}
+      <Page 
+        img={defaultBg.bg} 
+        alt={defaultBg.bg} 
+        info={defaultBg.bg_info}>
+          <h1>{defaultBg.name}</h1>
+          <p>{defaultBg.subtitle}</p>
+          <div className="collection-parent-container">
+            {mapping.map((card) => {
+                  return (
+                    <div className="flip-card-container">
+                      <Flippy 
+                        flipOnHover={true}
+                        flipDirection="horizontal"
+                        className="flip-card"
+                      >
+                        <FrontSide>
+                          <img 
+                            src       = {card.poster}
+                            alt       = {card.name}
+                            onClick   = {openPage(card)}
+                            className = {isMobile ? "card-img-mob" : "flip-front"} />
+                        </FrontSide>
+                        <BackSide>
+                          <div className="flip-back">
+                            <div className='flip-back-heading'>{card.name}</div>
+                            <p>{card.subtitle}</p>
+                          </div>
+                        </BackSide>
+                      </Flippy>
+                    </div>
+                  )
+              })}
+          </div>
       </Page>
   )
 }
